73d599b27fd899d1adbb6464cabb54d60d313d21
Robert Rothenberg <rrwo@cpan.org>
2018-11-18 20:43:17 +0000

    Bump version

--- lib/DBIx/Class/Helper/Row/Types.pm
+++ lib/DBIx/Class/Helper/Row/Types.pm
@@ -14,7 +14,7 @@ use Types::SQL::Util v0.3.0 ();
 # RECOMMEND PREREQ: Ref::Util::XS
 # RECOMMEND PREREQ: Type::Tiny::XS
 
-our $VERSION = 'v0.1.3';
+our $VERSION = 'v0.2.0';
 
 =for Pod::Coverage VERSION
 
